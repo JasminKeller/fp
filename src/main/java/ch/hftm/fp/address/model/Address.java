e113f6a3f1629d0a27c9ce59f3a0246bcd02aa6e
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-03-07 15:15:17 +0100

    Project-Structure + Dto

---
+++ src/main/java/ch/hftm/fp/address/model/Address.java
@@ -0,0 +1,4 @@
+package ch.hftm.fp.address.model;
+
+public class Address {
+}
---
+++ src/main/java/ch/hftm/fp/person/Person.java
@@ -0,0 +1,13 @@
+package ch.hftm.fp.person;
+
+import ch.hftm.fp.address.model.Address;
+
+import java.util.List;
+
+public class Person
+{
+    private String firstname;
+    private String lastname;
+    private Address mainAddress;
+    private List<Address> addresses;
+}
