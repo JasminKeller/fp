dbe88b35bd2a26f3604d2092c3027bfa2bfa202e
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-03-07 15:47:41 +0100

    PersonService + Jackson-Dependency

---
+++ src/main/java/ch/hftm/fp/person/PersonService.java
@@ -0,0 +1,21 @@
+package ch.hftm.fp.person;
+
+import ch.hftm.fp.person.model.Person;
+
+public class PersonService
+{
+    public Person getPerson()
+    {
+        return null;
+    }
+
+    public void savePerson( Person person )
+    {
+
+    }
+
+    public boolean deletePerson()
+    {
+        return false;
+    }
+}
--- src/main/java/ch/hftm/fp/person/model/Person.java
+++ src/main/java/ch/hftm/fp/person/model/Person.java
@@ -1,13 +1,16 @@
-package ch.hftm.fp.person;
+package ch.hftm.fp.person.model;
 
 import ch.hftm.fp.address.model.Address;
 
+import java.util.Date;
 import java.util.List;
 
 public class Person
 {
     private String firstname;
     private String lastname;
+
+    private Date birthdate;
     private Address mainAddress;
     private List<Address> addresses;
 }
