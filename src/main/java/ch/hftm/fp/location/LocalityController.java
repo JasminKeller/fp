4ef7754371581b075ef747943b35d61ba9353a8a
Jasmin Keller <jasmin.keller@hftm.ch>
2023-03-14 15:12:49 +0100

    LocalityController.java

---
+++ src/main/java/ch/hftm/fp/location/LocalityController.java
@@ -0,0 +1,51 @@
+package ch.hftm.fp.location;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import ch.hftm.fp.location.model.Locality;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LocalityController implements Initializable {
+
+    @FXML
+    private TextField plzTxt;
+
+    @FXML
+    private TextField  locationTxt;
+
+    @FXML
+    private ListView<?> locationPLZListView;
+
+
+    @FXML
+    void createNewLocationPLZ(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void saveLocationPLZ(ActionEvent event) {
+
+        var plz = plzTxt.getText();
+        var location = locationTxt.getText();
+
+        Locality locality = Locality.builder()
+            .plz(plz)
+            .location(location)
+            .build();
+        
+        new LocalityService().saveLocality(locality);
+
+    }
+
+
+}
