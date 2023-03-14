387618434281ab5e2cffde67c8a16d254dec2fd8
Jasmin Keller <jasmin.keller@hftm.ch>
2023-03-14 16:01:05 +0100

    showLocality added to adress controller

--- src/main/java/ch/hftm/fp/address/AddressController.java
+++ src/main/java/ch/hftm/fp/address/AddressController.java
@@ -3,9 +3,13 @@ package ch.hftm.fp.address;
 import ch.hftm.fp.address.model.Address;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,5 +37,24 @@ public class AddressController implements Initializable {
     public void createNewAddress( ActionEvent event )
     {
 
+    }
+
+    public void showLocality( ActionEvent actionEvent )
+    {
+        try
+        {
+            Stage stage = new Stage();
+            stage.setTitle("Adressen");
+
+            FXMLLoader loader = new FXMLLoader(App.class.getResource("view/Ortschaften.fxml"));
+            Scene scene = new Scene(loader.load(), 1100, 600);
+
+            stage.setScene( scene );
+            stage.show();
+        }
+        catch ( IOException e )
+        {
+            e.printStackTrace();
+        }
     }
 }
