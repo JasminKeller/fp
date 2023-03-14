4103c5007870153e13d33cebb0c05ea9012b4837
bryan.brotschi <bryan.brotschi@hftm.ch>
2023-03-14 14:42:02 +0100

    implements address functionality

--- src/main/java/ch/hftm/fp/address/model/Address.java
+++ src/main/java/ch/hftm/fp/address/model/Address.java
@@ -1,5 +1,18 @@
 package ch.hftm.fp.address.model;
+import ch.hftm.fp.locality.model.Locality;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class Address
 {
+    private String uuid;
+    private String street;
+    private List<Locality> localities;
 }
+
+
